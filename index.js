a8d88e78ebbf0877145507a5c8663ab3ddd315fd
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-22 19:00:04 +0800

    Set GET and POST only

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ var options = {
 
 app.use(morgan('combined'));
 app.use(function (req, res, next){
+    console.log(req.body);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DEconstE');
+    // res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DEconstE');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
     res.header('Access-Control-Expose-Headers', 'Content-Length');
     res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
     if (req.method === 'OPTIONS') {
