1979d374a2b641afeb469a5b3ab0c626238f42fb
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-23 10:57:38 +0800

    Revert auth for mongo

--- users/models/users.model.js
+++ users/models/users.model.js
@@ -13,7 +13,7 @@ const options = {
     socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
     family: 4 // Use IPv4, skip trying IPv6
 };
-mongoose.connect('mongodb://team14ssd_admin:DAzk_F9A@localhost:27017/bgd', options);
+mongoose.connect('mongodb://localhost:27017/bgd', options);
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
