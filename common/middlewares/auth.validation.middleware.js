75a53c397b01523b87bbdc2730b622f743fb35fb
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-26 19:22:03 +0800

    Auth error

--- common/middlewares/auth.validation.middleware.js
+++ common/middlewares/auth.validation.middleware.js
@@ -24,35 +24,42 @@ exports.validRefreshNeeded = (req, res, next) => {
 
 
 exports.validJWTNeeded = (req, res, next) => {
-    if (req.headers['authorization']) {
-        try {
-            let authorization = req.headers['authorization'].split(' ');
-            if (authorization[0] !== 'Bearer') {
-                return res.status(401).send({
+    if (req.headers['user-agent'].contains("Dalvik") && req.headers['user-agent'].contains("Android")) {
+        if (req.headers['authorization']) {
+            try {
+                let authorization = req.headers['authorization'].split(' ');
+                if (authorization[0] !== 'Bearer') {
+                    return res.status(401).send({
+                        "error": true,
+                        "message": 'Error.'
+                    });
+                } else {
+                    jwt.verify(authorization[1], secret, function (err, decoded) {
+                        if (err) {
+                            console.error("JWT invalid");
+                            return res.status(403).send(err);
+                        } else {
+                            req.jwt = jwt.verify(authorization[1], secret);
+                            return next();
+                        }
+                    });
+                }
+            } catch (err) {
+                return res.status(403).send({
                     "error": true,
-                    "message": 'Error.'
-                });
-            } else {
-                jwt.verify(authorization[1], secret, function (err, decoded) {
-                    if (err) {
-                        console.error("JWT invalid");
-                        return res.status(403).send(err);
-                    }else{
-                        req.jwt = jwt.verify(authorization[1], secret);
-                        return next();
-                    }
+                    "message": 'Token provided has errors.'
                 });
             }
-        } catch (err) {
+        } else {
             return res.status(403).send({
                 "error": true,
-                "message": 'Token provided has errors.'
+                "message": 'No token provided.'
             });
         }
-    } else {
-        return res.status(403).send({
+    }else{
+        return res.status(401).send({
             "error": true,
-            "message": 'No token provided.'
+            "message": 'Auth error.'
         });
     }
 };
