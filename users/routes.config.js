6b8d40f0796ffe5b343be3f82a566532fe297990
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-26 15:44:22 +0800

    TYPO

--- users/routes.config.js
+++ users/routes.config.js
@@ -9,7 +9,7 @@ const FREE = config.permissionLevels.NORMAL_USER;
 
 exports.routesConfig = function (app) {
     app.get('/.well-known/acme-challenge/llM7LhDy0hEh2qeffEBFSUWubJa197t6FTiDZxOin98', function(req, res) {
-        res.sendFile(__dirname+'./llM7LhDy0hEh2qeffEBFSUWubJa197t6FTiDZxOin98');
+        res.sendFile(__dirname+'/llM7LhDy0hEh2qeffEBFSUWubJa197t6FTiDZxOin98');
 
     });
     app.post('/bank', [
