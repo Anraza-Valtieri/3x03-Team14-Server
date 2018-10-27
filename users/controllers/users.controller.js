0ef0bba287ceba03daf8f94ccb5fd46bf53d6909
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-28 02:40:36 +0800

    Rex

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -19,26 +19,36 @@ let knownNumbers = [];
 exports.insert = (req, res) => {
     let salt = crypto.randomBytes(16).toString('base64');
 
-    if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.firstName) == false){
+    // if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.firstName) == true){
+    var firstNameReg = /^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/;
+    if(req.body.firstName.match(firstNameReg)){
         console.log("CANNOT firstName pattern does not match pattern " + req.body.firstName);
         return res.status(200).send({error: true, message: "firstName fail"});
     }
 
-    if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.lastName) == false){
+    // if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.lastName) == false){
+    let lastNameReg = /^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/;
+    if(req.body.lastName.match(lastNameReg)){
         console.log("CANNOT lastName pattern does not match pattern " + req.body.lastName);
         return res.status(200).send({error: true, message: "lastName fail"});
     }
 
-    if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/.test(req.body.email) == false){
+    // if(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/.test(req.body.email) == false){
+    let emailReg = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/;
+    if(req.body.email.match(emailReg)){
         console.log("CANNOT email pattern does not match pattern " + req.body.email);
         return res.status(200).send({error: true, message: "email fail"});
     }
-    if(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]).{8,50}$/.test(req.body.password) == false){
-        console.log("CANNOT Password pattern does not match pattern " + req.body.phoneNo);
+    // if(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]).{8,50}$/.test(req.body.password) == false){
+    let passReg = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[-!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]).{8,50}$/;
+    if(req.body.phoneNo.match(passReg)){
+        console.log("CANNOT Password pattern does not match pattern " + req.body.password);
         return res.status(200).send({error: true, message: "Password fail"});
     }
 
-    if(/^([89][0-9]{7})$/.test(req.body.phoneNo) == false){
+    // if(/^([89][0-9]{7})$/.test(req.body.phoneNo) == false){
+    let phoneReg = /^([89][0-9]{7})$/;
+    if(req.body.phoneNo.match(phoneReg)){
         console.log("CANNOT Phone does not match pattern " + req.body.phoneNo);
         return res.status(200).send({error: true, message: "phone fail"});
     }
