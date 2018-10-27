2434ebe457b803cccd9b5c03acd2e72352ff9160
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-27 23:44:00 +0800

    What?

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -363,7 +363,7 @@ exports.billConfirm = (req, res) => {
                     console.log("CLIENT -> SERVER (proceed to pay merchant)");
                     UserModel.findTransFromWithType(jwtResult.phoneNo, 4).then((trans) => {
                         console.log("PAY MERCH " + trans);
-                        let transId = trans._id;
+                        let transId = trans.__id;
                         console.log(transId);
                         if (trans != null && transId != null) {
                             console.log(jwtResult.phoneNo + " " + 4 + " : " + trans);
