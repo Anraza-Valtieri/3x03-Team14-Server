a42df9d70d8d216ac7421ea7aca5f790acc96180
Anraza Val'tieri <8533191+Anraza-Valtieri@users.noreply.github.com>
2018-10-28 22:19:35 +0800

    Revert "One last attempt"
    This reverts commit 333341ee3759d6bbd75f91ca5fbf25c6cf79394e.

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -804,17 +804,36 @@ exports.payMerchant = (req, res) => {
                                             });
                                         }else {
                                             console.log("req.body.splitBetween.includes(jwtResult.phoneNo.toString() FALSE");
-                                            var countingDown = req.body.splitAmount.length;
-                                            while (countingDown != -1){
-                                                if(knownNumbers.includes(req.body.splitBetween[countingDown])){
+                                            for (let z = 0; z < req.body.splitBetween.length; z++) {
+                                                console.log(z);
+                                                if (knownNumbers.includes(req.body.splitBetween[z])){
                                                     return res.status(200).send({
                                                         "error": true,
                                                         "message": 'Some phone numbers does not exist.',
-                                                        "numbers": transArray
+                                                        "numbers": req.body.splitBetween[z]
                                                     });
-                                                }else{
-                                                    countingDown = Number(countingDown)-Number(1);
-                                                    if(countingDown == -1){
+                                                }
+                                                // UserModel.findByPhone(req.body.splitBetween[z]).then((result) => {
+                                                //     if (result == null) {
+                                                //         console.log("We are missing this number " + req.body.splitBetween[z]);
+                                                //         // transArray.push(req.body.splitBetween[z]);
+                                                //         // callback();
+                                                //         return res.status(200).send({
+                                                //             "error": true,
+                                                //             "message": 'Some phone numbers does not exist.',
+                                                //             "numbers": req.body.splitBetween[z]
+                                                //         });
+                                                //     }
+                                                // });
+                                                if (z == req.body.splitBetween.length - 1) {
+                                                    console.log("Z-1");
+                                                    if (transArray.length > 0) {
+                                                        return res.status(200).send({
+                                                            "error": true,
+                                                            "message": 'Some phone numbers does not exist.',
+                                                            "numbers": transArray
+                                                        });
+                                                    } else {
                                                         console.log("Checking transaction 8s");
                                                         var results = UserModel.createTransaction(jwtResult.phoneNo,
                                                             jwtResult.phoneNo, sum, 8, detail.name);
