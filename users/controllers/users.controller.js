fb5bde5e5a235b133a21aa4e824c1ccd0ee5b4b9
ryancdy <31951010+ryancdy@users.noreply.github.com>
2018-10-28 20:59:00 +0800

    a little more

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -468,6 +468,10 @@ exports.payment = (req, res) => {
                                         console.log("1: " + result.balanceAmount);
                                         var addedAmt = (parseFloat(result.balanceAmount) + parseFloat(trans[0].amount)).toFixed(2);
                                         console.log("2: addedAmt: " + addedAmt);
+                                        UserModel.patchUser(result.id, {"balanceAmount": addedAmt}).then(() => {
+                                            console.log("3: patched");
+                                            
+                                        });
                                     }
                                 });
 
