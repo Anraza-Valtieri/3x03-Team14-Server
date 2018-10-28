c71da40afab248d88a52aa28ff9bd252ffcc9d8a
ryancdy <31951010+ryancdy@users.noreply.github.com>
2018-10-28 20:05:59 +0800

    sigh

--- users/controllers/users.controller.js
+++ users/controllers/users.controller.js
@@ -21,14 +21,14 @@ exports.insert = (req, res) => {
 
     // if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.firstName) == true){
     var firstNameReg = /^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+$/;
-    if(!req.body.firstName.match(firstNameReg) || req.body.firstName.length > 50){
+    if(!req.body.firstName.match(firstNameReg)){
         console.log("CANNOT firstName pattern does not match pattern " + req.body.firstName);
         return res.status(200).send({error: true, message: "firstName fail"});
     }
 
     // if(/^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+S/.test(req.body.lastName) == false){
     let lastNameReg = /^([a-zA-Z]+([ /]?[a-zA-Z]+)*)+$/;
-    if(!req.body.lastName.match(lastNameReg) || req.body.lastName.length > 50){
+    if(!req.body.lastName.match(lastNameReg)){
         console.log("CANNOT lastName pattern does not match pattern " + req.body.lastName);
         return res.status(200).send({error: true, message: "lastName fail"});
     }
@@ -460,30 +460,16 @@ exports.payment = (req, res) => {
                     }else {
                         let deductedAmt = parseFloat(jwtResult.balanceAmount).toFixed(2) - parseFloat(trans[0].amount).toFixed(2);
                         console.log(deductedAmt + " "+ jwtResult.balanceAmount + " "+trans[0].amount );
-                        UserModel.patchUser(jwtResult.id, {"balanceAmount": deductedAmt}).then(() => {
-                            // UserModel.patchTransaction(req.body.objectId, {type: req.body.request, read: true});
-                            UserModel.findByPhone(trans[0].fromId).then((result) => {
-                                console.log("------ 1");
-                                if (!result || result == null) {
-                                    console.log("------ 2");
-                                    res.status(200).send({
-                                        "error": true,
-                                        "message": 'Requester not found.'
-                                    });
-                                }
-                                console.log("------ 3");
-                                var addedAmt = (Number(result.balanceAmount) + Number(req.body.amount)).toFixed(2);
-                                console.log("------ 4");
-                                UserModel.patchUser(result.id, {"balanceAmount": addedAmt}).then(() => {
-                                    console.log("Transaction success!");
-                                    UserModel.patchTransaction(req.body.objectId, {type: 2});
-                                    return res.status(200).send({
-                                        "error": false,
-                                        "message": 'Transaction success.'
-                                    });
+                        UserModel.patchUser(jwtResult.id, {"balanceAmount": deductedAmt})
+                            .then(() => {
+                                // UserModel.patchTransaction(req.body.objectId, {type: req.body.request, read: true});
+                                console.log("Transaction success!");
+                                UserModel.patchTransaction(req.body.objectId, {type: 2});
+                                return res.status(200).send({
+                                    "error": false,
+                                    "message": 'Transaction success.'
                                 });
                             });
-                        });
                     }
                 });
             }
@@ -973,5 +959,4 @@ exports.request = (req, res) => {
         });
     }
 };
-
 
